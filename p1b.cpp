d833061a37383fe404909c8cbc91fdd54c740679
Altman Ma <gujiama@usc.edu>
2024-02-14 13:03:10 -0800

    p1b edit

--- p1b.cpp
+++ p1b.cpp
@@ -1,9 +1,9 @@
-#include <opencv2/opencv.hpp>
-#include <iostream>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
-#include <opencv2/photo.hpp>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
 using namespace cv;
 using namespace std;
