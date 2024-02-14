755f826c8dd0d2fefd916f75802b808c3a8015d9
AltmanMa <gujiama@usc.edu>
2024-02-14 13:10:35 -0800

    edited

--- p1b.cpp
+++ p1b.cpp
@@ -9,27 +9,38 @@ using namespace cv;
 using namespace std;
 
 int main(int argc, char** argv) {
-    // 加载图像
-    Mat img_tiger = imread("Tiger.jpg", IMREAD_GRAYSCALE);
-    Mat img_pig = imread("Pig.jpg", IMREAD_GRAYSCALE);
+    // 检查命令行参数数量是否正确
+    if (argc != 3) {
+        cout << "使用方法: " << argv[0] << " <输入图像文件名> <输出图像文件名>" << endl;
+        return -1;
+    }
 
-    if(img_tiger.empty() || img_pig.empty()) {
-        cout << "无法加载图像!" << endl;
+    // 读取用户输入的文件名
+    char* inputFile = argv[1];
+    char* outputFile = argv[2];
+
+    // 加载图像
+    Mat img = imread(inputFile, IMREAD_GRAYSCALE);
+
+    if(img.empty()) {
+        cout << "无法加载图像: " << inputFile << endl;
         return -1;
     }
 
     // 定义结果变量
-    Mat edges_tiger, edges_pig;
+    Mat edges;
 
     // 使用Canny边缘检测
-    Canny(img_tiger, edges_tiger, 100, 200); // 这里的100和200是双阈值参数
-    Canny(img_pig, edges_pig, 100, 200);
+    Canny(img, edges, 100, 200); // 这里的100和200是双阈值参数
 
-    // 显示结果
-    imshow("Edges of Tiger", edges_tiger);
-    imshow("Edges of Pig", edges_pig);
+    // 保存结果图像
+    bool isSuccess = imwrite(outputFile, edges);
+    if (!isSuccess) {
+        cout << "保存图像失败: " << outputFile << endl;
+        return -1;
+    }
 
-    waitKey(0); // 等待用户按键
+    cout << "图像处理成功，结果已保存到: " << outputFile << endl;
 
     return 0;
 }
